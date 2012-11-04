a430ace9c01380d6a80aab4a84d4e30c14cff308
Gergely POLONKAI <polesz@w00d5t0ck.info>
2012-11-04 14:12:03 +0100

    Made DatabaseAuth a valid, yet non-working authenticator

--- src/SmsGateway/Auth/DatabaseAuth.php
+++ src/SmsGateway/Auth/DatabaseAuth.php
@@ -6,4 +6,29 @@ use SmsGateway\LoggerInterface;
 
 class DatabaseAuth implements AuthInterface
 {
+    private $logger;
+
+    public function __construct(LoggerInterface $logger) {
+        throw new \Exception("This authenticator is not implemented yet!");
+    }
+
+    public function isTokenValid($token, $ip, $sessionId) {
+        return false;
+    }
+
+    public function getToken($username, $ip, $sessionId) {
+        return null;
+    }
+
+    public function removeToken($token, $ip, $sessionId) {
+        return true;
+    }
+
+    public function getLogger() {
+        return $this->logger;
+    }
+
+    public function setLogger(LoggerInterface $logger) {
+        $this->logger = $logger;
+    }
 }
